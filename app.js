0a46dba2adbaf50566f66197dbef13951759381e
ReoNaBear <adamsimprove1108@gmail.com>
2021-11-07 09:43:27 +0800

    feat:refactor routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
 
+const routes = require('./routes')
 const app = express()
 const PORT = 3000
 
@@ -11,10 +13,16 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+usePassport(app)
+
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
